34d14dfc458d4404f40422e10798c08c47227cf2
Keir Little <me@keirlittle.com>
2017-09-10 16:53:35 +0100

    it does the job

--- hangman.go
+++ hangman.go
@@ -9,6 +9,7 @@ import (
 )
 
 const min_word_length = 5
+const max_guesses = 10
 
 func check(e error) {
 	if e != nil {
@@ -33,11 +34,61 @@ func random_word(words []string) string {
 	return word
 }
 
+func print_guess_word(guess_word string) {
+	for _, char := range guess_word {
+		fmt.Printf("%c ", char)
+	}
+	fmt.Printf("\n")
+}
+
+func replaceAtIndex(str string, r rune, index int) string {
+	return str[:index] + string(r) + str[index+1:]
+}
+
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
 
 func main() {
 	word := random_word(dict_words())
-	fmt.Println(word)
+	length := len(word)
+	guess_word := strings.Repeat("_", length)
+
+	for guesses := max_guesses; guesses > 0; {
+		var guess string
+		fmt.Println("\n")
+		print_guess_word(guess_word)
+		fmt.Print("Enter guess (letter or whole word): ")
+		fmt.Scanln(&guess)
+
+		if len(guess) > 1 {
+			if guess == word {
+				fmt.Printf("Correct! The word was %s!\n", word)
+				break
+			} else {
+				guesses--
+				fmt.Printf("Incorrect! Guesses remaining: %d\n\n", guesses)
+			}
+		} else {
+			if strings.Contains(word, guess) {
+				for i, char := range word {
+					if char == []rune(guess)[0] {
+						guess_word = replaceAtIndex(guess_word, char, i)
+					}
+				}
+				if guess_word == word {
+					fmt.Printf("Success! %s completes the word %s!\n", guess, word)
+					break
+				} else {
+					fmt.Printf("%s is in the word.\n", guess)
+				}
+			} else {
+				guesses--
+				fmt.Printf("%s is not in the word. Guesses ramaining: %d\n\n", guess, guesses)
+			}
+		}
+	}
+	if guess_word != word {
+		fmt.Printf("The word was %s\n", word)
+	}
 }
